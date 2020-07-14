1fbe6e3026a22df96218f04662ee32e45a533aef
Ana Capa <anamaria.capa@epn.edu.ec>
2020-07-14 13:38:08 -0500

    Panda

---
+++ a_Introduccion.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 07:41:36 2020
+
+@author: LENOVO
+"""
+print("hola spider")
+
+
+nombre = "Ana"
+numero = 3
+lista = [1,2,3]
+tupla = (4,5,6)
+diccionrio = {
+    "nombre":"ana",
+     }
